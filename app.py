ab8198d9dc540568adcd2e5020ec728c5e326562
Makasevic <diego.ufrj@yahoo.com.br>
2024-12-27 11:19:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ curves_cons = curves.sum(axis=1).astype(int).reset_index()
 curves_cons.columns = ['Date', 'AUM']
 
 # Interface Streamlit
-tab1, tab2, tab3 = st.tabs(["Resultado consolidado", "Tabela de preços", "Tabela de cotas"])
+tab1, tab2, tab3, tab4 = st.tabs(["Evolução", "Proporção", "Tabela de preços", "Tabela de cotas"])
 
 with tab1:
     st.title("Evolução do patrimônio")
@@ -23,32 +23,18 @@ with tab1:
     consolidado = px.bar(curves_cons, x="Date", y="AUM", title="AUM Consolidado",color_discrete_sequence=["steelblue"])
     st.plotly_chart(consolidado, use_container_width=True)
 
-
-    # Certifique-se de que o índice foi resetado e nome correto é usado
     curves_reset = curves.reset_index()
     
-    curves_long = curves_reset.melt(
-        id_vars=[curves_reset.columns[0]],  # O nome correto da coluna do índice
-        var_name="Categoria",
-        value_name="Valor"
-    )
-    
-    # Criar o gráfico com todas as colunas em linhas
     st.subheader("Por classe")
-    plot = px.line(
-        curves_long,
-        x=curves_reset.columns[0],  # Use o índice resetado no eixo x
-        y="Valor",
-        color="Categoria",  # Diferencia por coluna original
-        title="Evolução por Categoria",
-        labels={curves_reset.columns[0]: "Data", "Valor": "AUM", "Categoria": "Classe"}
-    )
+    curves_long = curves_reset.melt(id_vars=[curves_reset.columns[0]],var_name="Categoria",value_name="Valor")
+    plot = px.line(curves_long,x=curves_reset.columns[0],y="Valor",color="Categoria",title="Evolução por Categoria",labels={curves_reset.columns[0]: "Data", "Valor": "AUM", "Categoria": "Classe"})
     st.plotly_chart(plot, use_container_width=True)
 
 
-    st.subheader("Por fundo")
-    fundo = px.bar(curves.reset_index(), x="index", y="Fund", title="AUM por Fundo",color_discrete_sequence=["steelblue"])
-    st.plotly_chart(fundo, use_container_width=True)
+    st.subheader("Por invetimento")
+    curves_long = curves_reset.melt(id_vars=[curves_reset.columns[0]],var_name="Categoria",value_name="Valor")
+    plot = px.line(curves_long,x=curves_reset.columns[0],y="Valor",color="Categoria",title="Evolução por Categoria",labels={curves_reset.columns[0]: "Data", "Valor": "AUM", "Categoria": "Classe"})
+    st.plotly_chart(plot, use_container_width=True)
 
 with tab2:
     st.title("Informações gerais")
