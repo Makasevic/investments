1882d78d5bb5738697cf742f5e04a09a019da999
Makasevic <diego.ufrj@yahoo.com.br>
2024-12-27 11:40:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,15 @@ curves_cons = curves.sum(axis=1).astype(int).reset_index()
 curves_cons.columns = ['Date', 'AUM']
 
 # Interface Streamlit
-tab1, tab2, tab3, tab4 = st.tabs(["Evolução", "Proporção", "Tabela de preços", "Tabela de cotas"])
+tab1, tab2, tab3, tab4 = st.tabs(["Evolução", "Composição", "Tabela de preços", "Tabela de cotas"])
 
 with tab1:
     st.title("Evolução do patrimônio")
     
-    st.subheader("Consolidado")
     consolidado = px.bar(curves_cons, x="Date", y="AUM", title="AUM Consolidado",color_discrete_sequence=["steelblue"])
     st.plotly_chart(consolidado, use_container_width=True)
 
     
-    st.subheader("Por classe")
     dict_names = dict(zip(invest_info['Bloomberg ID'], invest_info['Tipo']))
     curves_grouped = curves.rename(dict_names, axis=1)
     curves_grouped = curves_grouped.groupby(curves_grouped.columns, axis=1).sum()
@@ -33,8 +31,6 @@ with tab1:
     plot = px.line(curves_classe,x=curves_classe.columns[0],y="Valor",color="Categoria",title="Evolução por Categoria",labels={curves_classe.columns[0]: "", "Valor": "AUM", "Categoria": "Classe"})
     st.plotly_chart(plot, use_container_width=True)
 
-
-    st.subheader("Por invetimento")
     dict_names = dict(zip(invest_info['Bloomberg ID'], invest_info['Fundo']))
     curves_inv = curves.rename(dict_names, axis=1).fillna(0).reset_index()
     curves_inv = curves_inv.melt(id_vars=[curves_inv.columns[0]],var_name="Categoria",value_name="Valor")
@@ -43,7 +39,7 @@ with tab1:
 
 
 with tab2:
-    st.title("Informações gerais")
+    st.title("Composição")
         
     st.subheader("Por classe")
     dict_names = dict(zip(invest_info['Bloomberg ID'], invest_info['Tipo']))
